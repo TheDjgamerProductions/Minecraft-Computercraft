b2c3e2cdb70fb22a346545b28cc65b41900672d6
Dylan <40590952+TheDjgamerProductions@users.noreply.github.com>
2022-01-02 21:52:37 +1100

    Updated turtle file

--- Turtles/Farming/Turtle.lua
+++ Turtles/Farming/Turtle.lua
@@ -1,19 +1,32 @@
+turtle.select(1)
+rednet.open("right")
 
--- Varables
+
+-- User Modifyable Varables
 preSteps = 2
 steps = 7
+fuelSource = "minecraft:coal"
+seedType = "minecraft:wheat_seeds"
+cropType = "minecraft:wheat"
+rednetChnl = 10
+TurtleID = "Turtle 1"
+
+
+-- Non-User Modifyable Varables
 slot = 1
 stepsTaken = 0
+fuelLevel = 0
+allSteps = preSteps + steps
 
 
-
+-- Replants seeds
 function RePlant()
     slot = 1
-    while turtle.getItemDetail(slot).name ~= "minecraft:wheat_seeds" do --Select Seed
+    while turtle.getItemDetail(slot).name ~= seedType do --Select Seed
         slot = slot + 1
     end
-            turtle.select(slot)
-            print("Seeds Selected")
+    turtle.select(slot)
+    print("Seeds Selected")
     turtle.place()
 end
 
@@ -31,36 +44,69 @@ function drop()
 end
 
 
---Main Loop
-while 1 == 1 do
-while steps ~= stepsTaken do
-    print("Moving")
-    turtle.forward()
-    turtle.turnLeft()
-    isblock, Itemdata = turtle.inspect()
-    if (Itemdata.name == "minecraft:wheat") then
+--Check if more fuel is needed
+function checkFuel()
+    fuelLevel = turtle.getFuelLevel()
+    slot = 1
+    while fuelLevel < allSteps do
+        fuelLevel = turtle.getFuelLevel()
+        turtle.suckUp(1)
+        while turtle.getItemDetail(slot).name ~= fuelSource do --Select fuelSource
+            slot = slot + 1
+        end
+        turtle.select(slot)
+        turtle.refuel()
+    
+    
+    end
+end
+
+
+
+--Check if item infrount is wheat
+function checkGrowth()
+    if (Itemdata.name == cropType) then
         print("Block Infront")
         if (Itemdata.metadata == 7) then
             print("Full Grown")
             turtle.dig()
+            rednet.send(rednetChnl, TurtleID.." harvested a crop")
             RePlant()
         end
     end
-    turtle.turnRight()
-    stepsTaken = stepsTaken + 1
- end
-
- turtle.turnLeft()
- turtle.turnLeft()
- stepsTaken = 0
-
- while steps ~= stepsTaken do
-    turtle.forward()
-    stepsTaken = stepsTaken + 1
 end
+
+
+
+--Main Loop
+while 1 == 1 do
+    checkFuel()
+    while preSteps ~= stepsTaken do
+        turtle.forward()
+        stepsTaken = stepsTaken + 1
+    end
+    stepsTaken = 0
+    while steps ~= stepsTaken do
+        print("Moving")
+        turtle.forward()
+        turtle.turnLeft()
+        isblock, Itemdata = turtle.inspect()
+        checkGrowth()
+        turtle.turnRight()
+        stepsTaken = stepsTaken + 1
+    end
+
     turtle.turnLeft()
     turtle.turnLeft()
     stepsTaken = 0
-    drop()
-    os.sleep(300)
-end
+
+    while allSteps ~= stepsTaken do
+        turtle.forward()
+        stepsTaken = stepsTaken + 1
+    end
+        turtle.turnLeft()
+        turtle.turnLeft()
+        stepsTaken = 0
+        drop()
+        os.sleep(300)
+    end
